301dc0eff4091ace0cc99ce2ace41522167d741c
Ilackkeya Bhavananthi <bsilackkeya21@gmail.com>
2024-05-09 14:55:36 -0700

    Pokers_spade_updated-submission

--- Pokers_Spade_Game/pokers.py
+++ Pokers_Spade_Game/pokers.py
@@ -1,71 +1,48 @@
 
 # Declaring cards as tuples
 cards = ('S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'SJ', 'SQ', 'SK', 'SA')
-
+cards_dict = {'S2': 2, 'S3': 3, 'S4': 4, 'S5': 5, 'S6': 6, 'S7': 7, 'S8': 8, 'S9': 9,
+              'S10': 10, 'SJ': 11, 'SQ': 12, 'SK': 13, 'SA': 14}
 
 # Define check_straight function
 def check_straight(card1, card2, card3):
-
-    # map every item in cards to corresponding number from 1 to 14
-    cards_dict = {'S2': 2, 'S3': 3, 'S4': 4, 'S5': 5, 'S6': 6, 'S7': 7, 'S8': 8, 'S9': 9,
-                  'S10': 10, 'SJ': 11, 'SQ': 12, 'SK': 13, 'SA': 14}
+    card_val1 = cards_dict[card1]
+    card_val2 = cards_dict[card2]
+    cards_val3 = cards_dict[card3]
     # creating list of cards
-    player_card = [card1, card2, card3]
-    lst_card = []
-    # check if they are 3 consecutive numbers
-    for i in range(len(player_card)):  # looping through player_card list
-        for key, value in cards_dict.items():
-            if key == player_card[i]:  # checking in cards_dictionary
-                card_val = value
-                lst_card.append(card_val)  # appending the values in a list
-
-    # sorting and matching for consecutive numbers in the list
-    if sorted(lst_card) == list(range(min(lst_card), max(lst_card)+1)):
-        return max(lst_card)  # return highest value among the three cards
+    player_card = [card_val1, card_val2, cards_val3]
+    cards = sorted(player_card)
+    if cards[2] == cards[1]+1 and cards[1] == cards[0]+1:
+        return max(cards)
     else:
         return 0
 
 
 # Define check_3ofa_kind function
 def check_3ofa_kind(card1, card2, card3):
-    # map every item in cards to corresponding number from 1 to 14
-    cards_dict = {'S2': 2, 'S3': 3, 'S4': 4, 'S5': 5, 'S6': 6, 'S7': 7, 'S8': 8, 'S9': 9,
-                  'S10': 10, 'SJ': 11, 'SQ': 12, 'SK': 13, 'SA': 14}
-    player_card = [card1, card2, card3]
-    player_card.sort()
-    # checking if all three are same and then return value
-    for i in range(len(player_card) - 2):
-        if player_card[i] == player_card[i + 1] and player_card[i] == player_card[i + 2]:
-            common_value = player_card[i]
-            for key, value in cards_dict.items():  # matching common_value in dictionary keys and returning the value
-                if common_value == key:
-                    card_value = cards_dict.get(key)
-            return card_value
-
-        else:  # else return 0
-            return 0
+    card_val1 = cards_dict[card1]
+    card_val2 = cards_dict[card2]
+    cards_val3 = cards_dict[card3]
+    # creating list of cards
+    player_card = [card_val1, card_val2, cards_val3]
+    cards = sorted(player_card)
+    if cards[0] == cards[1] == cards[2]:
+        return cards[1]
+    else:
+        return 0
 
 
 # Define check_royal_flush
 def check_royal_flush(card1, card2, card3):
-
-    return_value = check_straight(card1, card2, card3)
     # and if value = 14, return 14
-    if return_value == 14:
+    if check_straight(card1, card2, card3) == 14:
         return 14
     # else return 0
     else:
         return 0
 
-
 # Define play_cards
 def play_cards(left1, left2, left3, right1, right2, right3):
-
-    left_chk_strt = check_straight(left1, left2, left3)
-    right_chk_strt = check_straight(right1, right2, right3)
-    print("Check_straight \n")
-    print(left_chk_strt)
-    print(right_chk_strt)
 
     left_chk_3 = check_3ofa_kind(left1, left2, left3)
     right_chk_3 = check_3ofa_kind(right1, right2, right3)
@@ -73,29 +50,20 @@ def play_cards(left1, left2, left3, right1, right2, right3):
     print(left_chk_3)
     print(right_chk_3)
 
+    left_chk_strt = check_straight(left1, left2, left3)
+    right_chk_strt = check_straight(right1, right2, right3)
+    print("Check_straight \n")
+    print(left_chk_strt)
+    print(right_chk_strt)
+
     left_ryl = check_royal_flush(left1, left2, left3)
     right_ryl = check_royal_flush(right1, right2, right3)
     print("Check_royal_flush \n")
     print(left_ryl)
     print(right_ryl)
 
-    # Determining winner
-    print("The winner is: \n")
-
-    # if both players play straight, highest value wins
-    if left_chk_strt == True and right_chk_strt == True:
-        if left_chk_strt > right_chk_strt:
-            print("Left wins!")
-            return -1
-        elif right_chk_strt > left_chk_strt:
-            print("Right wins!")
-            return 1
-        else:
-            print("Draw")
-            return 0
-
     # if both play 3-of-a-kind, highest value wins
-    if left_chk_3 == True and right_chk_3 == True:
+    if left_chk_3 >0  and right_chk_3 > 0:
         if left_chk_3 > right_chk_3:
             print("Left wins!")
             return -1
@@ -107,30 +75,21 @@ def play_cards(left1, left2, left3, right1, right2, right3):
             return 0
 
     # if one player plays a straight and the other has a three-of-a-kind, the straight wins regardless of value
-    if left_chk_strt == True and right_chk_3 == True:
+    if left_chk_strt >0  and right_chk_3 >0:
         print("Left wins!")
         return -1
-    elif right_chk_strt == True and left_chk_3 == True:
+    if right_chk_strt >0 and left_chk_3 >0:
         print("Right wins!")
         return 1
-    else:
-        print("Draw")
-        return 0  # In all other cases, it’s a draw.
 
     # if one player plays a royal flush and the other does not, the flush wins.
-    if left_ryl == True and right_ryl == False:
+    if left_ryl == 14 and right_ryl == 0:
         print("Left wins!")
         return -1
-    else:
-        print("Draw")
-        return 0  # In all other cases, it’s a draw.
-
-    if left_ryl is False and right_ryl is True:
+    if right_ryl == 14 and left_ryl ==0:
         print("Right wins!")
         return 1
-    else:
-        print("Draw")
-        return 0  # In all other cases, it’s a draw.
 
+    #everything else return 0
+    return 0
 
-play_cards('S5', 'S5', 'S5', 'S6', 'S6', 'S6')
--- Pokers_Spade_Game/test_pokers.py
+++ Pokers_Spade_Game/test_pokers.py
@@ -1,22 +1,71 @@
 import unittest
 import pokers
 
+SA = 'SA'
+SK = 'SK'
+SQ = 'SQ'
+SJ = 'SJ'
+S2 = 'S2'
+S3 = 'S3'
+S4 = 'S4'
+S5 = 'S5'
+S6 = 'S6'
+S7 = 'S7'
+S8 = 'S8'
+S9 = 'S9'
+S10 = 'S10'
+
 
 class MyTestCase(unittest.TestCase):
-    # def test_something(self):
+    #def test_something(self):
     #     self.assertEqual(True, False)  # add assertion here
+    def test_check_straight1(self):
+         self.assertEqual(pokers.check_straight(S5, S6, S7), 7)
+    def test_check_straight2(self):
+        self.assertEqual(pokers.check_straight(S4, S6, S5), 6)
+    def test_check_straight3(self):
+        self.assertEqual(pokers.check_straight(SQ, SA, SK), 14)
+    def test_check_straight4(self):
+        self.assertEqual(pokers.check_straight(S5, S9, SA), 0)
+    def test_check_straight5(self):
+        self.assertEqual(pokers.check_straight(S2, S3, S5), 0)
+    def test_check_3ofakind1(self):
+        self.assertEqual(pokers.check_3ofa_kind(S2, S2, S2), 2)
+    def test_check_3ofakind2(self):
+        self.assertEqual(pokers.check_3ofa_kind(S4, S2, S6), 0)
+    def test_check_3ofakind3(self):
+        self.assertEqual(pokers.check_3ofa_kind(S4, S4, S6), 0)
+    def test_check_3ofakind4(self):
+        self.assertEqual(pokers.check_3ofa_kind(S5, S5, S5), 5)
+    def test_royal_flush1(self):
+        self.assertEqual(pokers.check_royal_flush(SQ, SK, SA), 14)
+    def test_royal_flush2(self):
+        self.assertEqual(pokers.check_royal_flush(S2, S3, S5), 0)
+    def test_royal_flush3(self):
+        self.assertEqual(pokers.check_royal_flush(SJ, SQ, SK), 0)
+    def test_royal_flush4(self):
+        self.assertEqual(pokers.check_royal_flush(SA, SA, SA), 0)
 
-    # def test_check_straight(self):.
-    #     self.assertEqual(pokers.check_straight('S5','S6','S7'), 1)
-    #
-    #
-    # def test_check_straight(self): .
-    #     self.assertEqual(pokers.check_straight('S5', 'S6', 'S7'), 1)
-    #
-    #
-    # def test_check_straight(self): .
-    #     self.assertEqual(pokers.check_straight('S5', 'S6', 'S7'), 1)
+    def test_play_cards1(self):
+        self.assertEqual(pokers.play_cards(S5, S5, S5, S2, S2, S2), -1)
+    def test_play_cards2(self):
+        self.assertEqual(pokers.play_cards(S4, S4, S4, S7, S7, S7), 1)
+    def test_play_cards3(self):
+        self.assertEqual(pokers.play_cards(S3, S4, S5, S2, S2, S2), -1)
+    def test_play_cards4(self):
+        self.assertEqual(pokers.play_cards(S3, S4, S5, S3, S4, S5), 0)
+    def test_play_cards5(self):
+        self.assertEqual(pokers.play_cards(SK, SQ, SA, S2, S2, S2), -1)
+    def test_play_cards6(self):
+        self.assertEqual(pokers.play_cards(S6, S5, S7, SK, SQ, SA), 1)
+    def test_play_cards7(self):
+        self.assertEqual(pokers.play_cards(S4, S5, S3, SK, SQ, SA), 1)
 
+    def test_play_cards8(self):
+        self.assertEqual(pokers.play_cards(S5, S5, S5, S4, S2, SK), 0)
+
+    def test_play_cards9(self):
+        self.assertEqual(pokers.play_cards(S2, S5, S4, SK, S10, S6), 0)
 
 if __name__ == '__main__':
     unittest.main()
