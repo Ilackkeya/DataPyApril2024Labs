42e20b19aa6a8519e844164add1f868580b3d11e
Ilackkeya <bsilackkeya21@gmail.com>
2024-06-12 13:35:24 -0700

    Public Transit Database - ETL lab

---
+++ ETL_PublicTransitDatabase/Create_tables.sql
@@ -0,0 +1,7 @@
+-- CREATE TABLE bus_monthly_totals
+--   AS (SELECT *
+--       FROM monthly_totals WHERE transportation_type = 'bus');
+
+-- CREATE TABLE metro_monthly_totals
+--   AS (SELECT *
+--       FROM monthly_totals WHERE transportation_type = 'metro');
